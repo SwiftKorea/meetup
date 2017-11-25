c62b2e879ccdc6d83590252523d9b00b9d168d24
Suyeol Jeon <devxoul@gmail.com>
2017-11-26 03:16:21 +0900

    2회 초안 작성

---
+++ meetups/2/js/script.js
@@ -0,0 +1,50 @@
+var coordinate = new daum.maps.LatLng(37.506105, 127.063824);
+
+function drawMap() {
+  var container = document.getElementById('map');
+
+  var options = {
+    center: coordinate,
+    level: 4
+  };
+  var map = new daum.maps.Map(container, options);
+  map.setZoomable(false);
+
+  var markerPosition = coordinate;
+  var marker = new daum.maps.Marker({
+    position: markerPosition
+  });
+  marker.setMap(map);
+
+  var zoomControl = new daum.maps.ZoomControl();
+  map.addControl(zoomControl, daum.maps.ControlPosition.RIGHT);
+
+  var iwContent = $('#info-window-template').html();
+  var iwPosition = coordinate;
+  var infowindow = new daum.maps.InfoWindow({
+    position: iwPosition,
+    content: iwContent
+  });
+  infowindow.open(map, marker);
+  return map;
+}
+
+function updateMapDraggable(map) {
+  var width = $(window).width();
+  var draggable = width > 768;
+  map.setDraggable(draggable);
+}
+
+$(document).ready(function() {
+  $('.btn-onoffmix').click(function() {
+    fbq('track', 'ViewContent');
+  });
+
+  var map = drawMap();
+  updateMapDraggable(map);
+
+  $(window).resize(function() {
+    updateMapDraggable(map);
+    map.setCenter(coordinate);
+  });
+});
